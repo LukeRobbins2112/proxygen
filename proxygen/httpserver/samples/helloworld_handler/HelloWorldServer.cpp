d10928f0e6191006b7744f42e960fe827b06b63a
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-11-26 17:38:30 -0500

    Removed unnecessary SPDY socket

--- proxygen/httpserver/samples/helloworld_handler/HelloWorldServer.cpp
+++ proxygen/httpserver/samples/helloworld_handler/HelloWorldServer.cpp
@@ -23,7 +23,6 @@ using folly::SocketAddress;
 using Protocol = HTTPServer::Protocol;
 
 DEFINE_int32(http_port, 11000, "Port to listen on with HTTP protocol");
-DEFINE_int32(spdy_port, 11001, "Port to listen on with SPDY protocol");
 DEFINE_int32(h2_port, 11002, "Port to listen on with HTTP/2 protocol");
 DEFINE_string(ip, "localhost", "IP/Hostname to bind to");
 DEFINE_int32(threads, 0, "Number of threads to listen on. Numbers <= 0 "
@@ -51,8 +50,9 @@ int main(int argc, char* argv[]) {
     google::InstallFailureSignalHandler();
 
     std::vector<HTTPServer::IPConfig> IPs = {
+            // FLAGS_ip / FLAGS_http_port are gflags
+            // could set via command line, or else it will use the default defined above
             {SocketAddress(FLAGS_ip, FLAGS_http_port, true), Protocol::HTTP},
-            {SocketAddress(FLAGS_ip, FLAGS_spdy_port, true), Protocol::SPDY},
             {SocketAddress(FLAGS_ip, FLAGS_h2_port, true), Protocol::HTTP2},
     };
 
